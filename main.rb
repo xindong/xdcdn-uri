a1f257eb18e405db179280ae86d23ce5a3531120
xdanger <xdanger@gmail.com>
2012-08-29 05:33:34 +0800

    index 加上 gzip

--- main.rb
+++ main.rb
@@ -5,11 +5,12 @@ require 'sinatra'
 require 'pp'
 require 'xdcdn_uri'
 require 'radix62'
+require 'zlib'
 
 APP_ROOT = File.dirname(__FILE__)
 
 set :port, 3003
-set :public, APP_ROOT + '/public'
+set :public_folder, APP_ROOT + '/public'
 
 def xdcdn(dir)
     XdcdnUri.new(dir)
@@ -17,11 +18,11 @@ end
 
 configure :production do
     set :ktk, xdcdn("/home/www/sites/ktk.xdcdn.net")
-done
+end
 
 configure :development do
     set :ktk, xdcdn("/Users/xdanger/Sites/xdcdn.net/ktk.xdcdn.net")
-done
+end
 
 def pack_trees_hash(trees_hash)
     index = []
@@ -35,9 +36,10 @@ end
 
 get '/ktk/index/:tag' do
     begin
-        idx = settings.ktk.index("20120829A")
+        idx = settings.ktk.index(params[:tag])
         response['Cache-Control'] = 'max-age=31536000'
-        pack_trees_hash(idx)
+        data = pack_trees_hash(idx)
+        Zlib::Deflate.deflate(data, 9)
     rescue
         response['Cache-Control'] = 'max-age=0'
         "Error"
