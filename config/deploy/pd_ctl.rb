4ad6e4315a1e64a1fa498569a677914b4a81e532
xdanger <xdanger@gmail.com>
2014-02-20 11:53:53 +0800

    bugfix: capistrano config

--- config/deploy/pd_ctl.rb
+++ config/deploy/pd_ctl.rb
@@ -1,11 +1,12 @@
 set :stage, :pd_ctl
-server '116.182.64.232', user: 'root', port: 22281, roles: %w{nodes_clt}
-server '116.182.64.232', user: 'root', port: 22291, roles: %w{nodes_clt}
-server '116.182.64.232', user: 'root', port: 22301, roles: %w{nodes_clt}
-server '223.202.26.147', user: 'root', port: 22   , roles: %w{nodes_clt}
-server '223.202.26.148', user: 'root', port: 22   , roles: %w{nodes_clt}
-server '223.202.26.149', user: 'root', port: 22   , roles: %w{nodes_clt}
-server '60.251.11.28'  , user: 'root', port: 22   , roles: %w{nodes_clt}
-server '60.251.11.29'  , user: 'root', port: 22   , roles: %w{nodes_clt}
-server '162.243.140.6' , user: 'root', port: 22   , roles: %w{nodes_ctt}
+server '116.182.64.232', user: 'root', port: 22281, roles: %w{nodes_ctl}
+server '116.182.64.232', user: 'root', port: 22291, roles: %w{nodes_ctl}
+server '116.182.64.232', user: 'root', port: 22301, roles: %w{nodes_ctl}
+server '223.202.26.147', user: 'root', port: 22   , roles: %w{nodes_ctl}
+server '223.202.26.148', user: 'root', port: 22   , roles: %w{nodes_ctl}
+server '223.202.26.149', user: 'root', port: 22   , roles: %w{nodes_ctl}
+server '60.251.11.28'  , user: 'root', port: 22   , roles: %w{nodes_ctl}
+server '60.251.11.29'  , user: 'root', port: 22   , roles: %w{nodes_ctl}
+server '106.186.22.159', user: 'root', port: 22   , roles: %w{nodes_ctl}
+server '162.243.140.6' , user: 'root', port: 22   , roles: %w{nodes_ctl}
 
--- config/deploy/pd_www.rb
+++ config/deploy/pd_www.rb
@@ -1,12 +1,13 @@
 set :stage, :pd_www
 #role :nodes_clt, %w{ root@116.182.64.232:22281 root@115.182.64.232:22291 root@115.182.64.232:22301 root@60.251.11.28 root@60.251.11.29 }
 #role :nodes_www, %w{  www@115.182.64.232:22281  www@115.182.64.232:22291  www@115.182.64.232:22301  www@60.251.11.28  www@60.251.11.29 }
-server '116.182.64.232', user: 'www' , port: 22281, roles: %w{nodes_www}
-server '116.182.64.232', user: 'www' , port: 22291, roles: %w{nodes_www}
-server '116.182.64.232', user: 'www' , port: 22301, roles: %w{nodes_www}
+server '115.182.64.227', user: 'www' , port: 22281, roles: %w{nodes_www}
+server '115.182.64.227', user: 'www' , port: 22291, roles: %w{nodes_www}
+server '115.182.64.227', user: 'www' , port: 22301, roles: %w{nodes_www}
 server '223.202.26.147', user: 'www' , port: 22   , roles: %w{nodes_www}
 server '223.202.26.148', user: 'www' , port: 22   , roles: %w{nodes_www}
 server '223.202.26.149', user: 'www' , port: 22   , roles: %w{nodes_www}
 server '60.251.11.28'  , user: 'www' , port: 22   , roles: %w{nodes_www}
 server '60.251.11.29'  , user: 'www' , port: 22   , roles: %w{nodes_www}
+server '106.186.22.159', user: 'www' , port: 22   , roles: %w{nodes_www}
 server '162.243.140.6' , user: 'www' , port: 22   , roles: %w{nodes_www}
